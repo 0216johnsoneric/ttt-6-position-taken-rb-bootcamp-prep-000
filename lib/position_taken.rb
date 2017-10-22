8dadaf9b77712e1726416ca8fbdcb0020766a0cf
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-22 19:21:34 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] === " "
   return false
-  if board[index] === ""
+  else board[index] === ""
   return false
 # else board["X" or "O"]
 #   return true
